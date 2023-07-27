62520db401966d5e58543adcec6f6904f618bf16
robin <hongxing525@126.com>
2023-07-28 01:50:33 +0800

    升级sd1.5.1

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# https://docs.docker.com/compose/install/linux/
+
+docker compose version
+
+
+ 2015  docker compose --profile download up --build
+ 2016  docker images
+ 2017  docker compose --profile auto up --build
+ 2018  docker images
+ 2019  df -h
+ 2020  pwd
+ 2021  ls
+ 2022  cd services/
+ 2023  ls
+ 2024  vi AUTOMATIC1111/Dockerfile 
+ 2025  cd ..
+ 2026  docker compose --profile auto up --build
+ 2027  dokcer images
+ 2028  docker images
+ 2029  docker run --rm sd-auto:62 /bin/sh
+ 2030  pwd
+ 2031  docker run --rm -it sd-auto:62 /bin/sh
+ 2032  docker images
+ 2033  pwd
+ 2034  vi AUTOMATIC1111/Dockerfile 
+ 2035  vi services/AUTOMATIC1111/Dockerfile 
+ 2036  docker compose --profile auto up --build
+ 2037  docker run --rm -it sd-auto:62 /bin/sh
+ 2038  vi services/AUTOMATIC1111/Dockerfile 
+ 2039  docker compose --profile auto up --build
+ 2040  vi services/AUTOMATIC1111/Dockerfile 
+ 2041  docker compose --profile auto up --build
+ 2042  vi services/AUTOMATIC1111/Dockerfile 
+ 2043  docker compose --profile auto up --build
+ 2044  docker run --rm -it sd-auto:62 /bin/sh
+ 2045  vi services/AUTOMATIC1111/Dockerfile 
+ 2046  docker compose --profile auto up --build
+ 2047  docker images
+ 2048  docker run --rm -it sd-auto:62 /bin/sh
+ 2049  history
+ 2050  history | awk '{print $2}'
+ 2051  history
